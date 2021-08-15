3506ad8354deaeca85f853a46807863fc61b4639
xMinota <jpichardo369@gmail.com>
2021-08-15 05:39:05 -0400

    Add basic Next.js stuff

---
+++ src/components/Container.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Container(props) {
+    const { children, ...customMeta } = props
+    return (
+        <>
+            <Head>
+                <title>minota's website</title>
+                <meta content="minota" name="description"/>
+                <meta name="theme-color" content="#ff9cfa"/>
+            </Head>
+            <body>
+                <main>
+                    {children}
+                </main>
+            </body>
+        </>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Container from "../components/Container";
+
+export default function Home() {
+    return (
+        <Container>
+            <div className="md:container md:mx-auto">
+                <h1>Hello!</h1>
+                <hr/>
+            </div>
+        </Container>
+    )
+}
